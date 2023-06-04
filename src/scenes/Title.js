6ee4ff4955b161bae52fde049a988795176bf1fc
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-06-03 23:56:01 -0700

    adding temp titlescreen, init prefabs

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -1,3 +1,22 @@
-class Title extends Phaser.Scene{
-    
+class Title extends Phaser.Scene {
+    constructor(){
+        super("menuScene");
+    }
+    preload(){
+        this.load.image('title', './assets/temp_title.png');
+    }
+
+    create(){
+        // adding title screen
+        this.title = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'title').setOrigin(0, 0);
+
+        // defining keys
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    }
+
+    update(){
+        if(Phaser.Input.Keyboard.JustDown(keySPACE)){ 
+            this.scene.start('tutorial');
+        }
+    }
 }
