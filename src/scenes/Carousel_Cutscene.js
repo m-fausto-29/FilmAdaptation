9488816bb047ec591902d3b36a420571478ba2d7
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2023-06-04 17:03:09 -0700

    implementing carousel scenes

--- src/scenes/Carousel_Cutscene.js
+++ src/scenes/Carousel_Cutscene.js
@@ -4,18 +4,20 @@ class Carousel_Cutscene extends Phaser.Scene {
     }
     preload(){
         this.load.image('cutscene', './assets/temp_cutscene.png');
+        this.load.audio('beep1', './assets/temp_beep1.wav');
     }
 
     create(){
-        // adding cutscene 2
-        this.cutscene2 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'cutscene').setOrigin(0, 0);
+        // adding cutscene 1
+        this.cutscene1 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'cutscene').setOrigin(0, 0);
 
         // defining keys
-        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
     }
 
     update(){
-        if(Phaser.Input.Keyboard.JustDown(keySPACE)){
+        if(Phaser.Input.Keyboard.JustDown(keyENTER)){
+            this.sound.play('beep1');
             this.scene.start('Carousel');
         }
     }
